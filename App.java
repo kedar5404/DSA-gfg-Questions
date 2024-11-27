ec60b96dc08f754ab01016b8ba697ccbc569403a
kedar5404 <shrutippatil4@gmail.com>
2024-11-27 11:38:52 +0530

    Added java files

---
+++ App.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
